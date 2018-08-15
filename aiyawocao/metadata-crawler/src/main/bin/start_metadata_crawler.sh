ccb037564bb4e7f6de554e8406e7cd25c800da06
caojianhua <caojianhua@bytedance.com>
2018-08-15 21:13:08 +0800

    modify start bash
    Change-Id: Iebe36c36aba23370f214fc1fd50801677b07eee7

--- aiyawocao/metadata-crawler/src/main/bin/start_metadata_crawler.sh
+++ aiyawocao/metadata-crawler/src/main/bin/start_metadata_crawler.sh
@@ -8,7 +8,7 @@ export PATH=$PATH:$JAVA_HOME/bin
 export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
 
 main_class=com.killxdcj.aiyawocao.metadata.crawler.MetaCrawlerMain
-jvm_opts="-Xmx250m -Xms250m -XX:+PrintGCDateStamps -XX:+PrintGCDetails -Xloggc:$work_dir/gc.log"
+jvm_opts="-Xmx150m -Xms150m -Xmn50m -XX:SurvivorRatio=4 -XX:+PrintGCDateStamps -XX:+PrintGCDetails -Xloggc:$work_dir/gc.log"
 logconf_path=$work_dir/conf/log4j2.xml
 crawler_conf=$work_dir/conf/metadata-crawler.yaml
 lib_path=$work_dir/lib/metadata-crawler-1.1-SNAPSHOT.jar
--- aiyawocao/metadata-service-server/src/main/bin/start_metadata_service.sh
+++ aiyawocao/metadata-service-server/src/main/bin/start_metadata_service.sh
@@ -8,7 +8,7 @@ export PATH=$PATH:$JAVA_HOME/bin
 export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
 
 main_class=com.killxdcj.aiyawocao.metadata.service.server.MetadataServiceServerMain
-jvm_opts="-Xmx200m -Xms200m -XX:+PrintGCDateStamps -XX:+PrintGCDetails -Xloggc:$work_dir/gc.log"
+jvm_opts="-Xmx110m -Xms110m -Xmn40m -XX:+PrintGCDateStamps -XX:+PrintGCDetails -Xloggc:$work_dir/gc.log"
 logconf_path=$work_dir/conf/log4j2.xml
 proxy_conf=$work_dir/conf/metadata-service.yaml
 lib_path=$work_dir/lib/metadata-service-server-1.1-SNAPSHOT.jar

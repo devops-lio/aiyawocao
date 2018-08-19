e4e9d4b727e5c37aa0d9f1e6d5418acff63d2707
killxdcj <caojian0613@163.com>
2018-08-20 00:52:28 +0800

    add web html

--- aiyawocao/aiyawocao-web/src/main/java/com/killxdcj/aiyawocao/web/controller/WebController.java
+++ aiyawocao/aiyawocao-web/src/main/java/com/killxdcj/aiyawocao/web/controller/WebController.java
@@ -3,8 +3,6 @@ package com.killxdcj.aiyawocao.web.controller;
 import com.killxdcj.aiyawocao.web.model.Metadata;
 import com.killxdcj.aiyawocao.web.service.ESService;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -27,19 +26,24 @@ public class WebController {
     return "hello";
   }
 
-  @RequestMapping("search/{key}")
-  public String search(@PathVariable String key, Model model) {
-    return "";
+  @RequestMapping("search")
+  public String search(@RequestParam String keyword, Model model) {
+    try {
+      Object result = esService.search(keyword, 0, 10);
+      return null;
+    } catch (IOException e) {
+      return "home";
+    }
   }
 
   @RequestMapping("detail/{infohash}")
   public String detail(@PathVariable String infohash, Model model) {
     try {
       model.addAttribute("metadata", new Metadata(esService.detail(infohash)));
-      return "detail";
+      return "test";
     } catch (IOException e) {
       LOGGER.error("query infohash error", e);
-      return "index";
+      return "home";
     }
 
   }

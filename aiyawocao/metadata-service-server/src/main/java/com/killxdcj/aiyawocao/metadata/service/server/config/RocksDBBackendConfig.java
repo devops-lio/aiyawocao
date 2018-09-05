4f670a3bc3b66eb7312658d7a057d894647582a5
caojianhua <caojianhua@bytedance.com>
2018-09-05 18:42:06 +0800

    optimze metadata service, add bitmap filter
    Change-Id: Ie5204a17829339ad17b73fe843128f293efcbbd4

--- aiyawocao/metadata-service-server/src/main/java/com/killxdcj/aiyawocao/metadata/service/server/config/RocksDBBackendConfig.java
+++ aiyawocao/metadata-service-server/src/main/java/com/killxdcj/aiyawocao/metadata/service/server/config/RocksDBBackendConfig.java
@@ -4,6 +4,7 @@ public class RocksDBBackendConfig {
 
   private String rocksDBPath = "/data/aiyawocao/metadata/index";
   private String originalMetadataPath = "/data/aiyawocao/metadata/original";
+  private boolean enableBitfilter = true;
 
   public String getRocksDBPath() {
     return rocksDBPath;
@@ -21,11 +22,20 @@ public class RocksDBBackendConfig {
     this.originalMetadataPath = originalMetadataPath;
   }
 
+  public boolean getEnableBitfilter() {
+    return enableBitfilter;
+  }
+
+  public void setEnableBitfilter(boolean enableBitfilter) {
+    this.enableBitfilter = enableBitfilter;
+  }
+
   @Override
   public String toString() {
     return "RocksDBBackendConfig{" +
         "rocksDBPath='" + rocksDBPath + '\'' +
         ", originalMetadataPath='" + originalMetadataPath + '\'' +
+        ", enableBitfilter=" + enableBitfilter +
         '}';
   }
 }

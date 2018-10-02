16bf676d3790feee1b97b8b733c8ab6055503984
caojianhua <caojianhua@bytedance.com>
2018-10-02 17:01:15 +0800

    optimze log
    Change-Id: Id810eafe2de83a3fc412edbe9a174e5b51517e9d

--- es_op/reindex.sh
+++ es_op/reindex.sh
@@ -3,21 +3,21 @@ dates=`ls /data/metadata/archive/human/ | awk -F '.' '{print $2}' | sort | uniq`
 for date in $dates
 do
     unalias rm
-    echo "reindex start "$date
+    echo `date`": reindex start "$date
     datedir="metadata."$date
     mkdir -p $datedir
     for file in `ls /data/metadata/archive/human/ | grep $date`
     do
-        echo "uncompress start "$file
+        echo `date`": uncompress start "$file
         unzip -q /data/metadata/archive/human/$file -d $datedir
-        echo "uncompress finished "$file
+        echo `date`": uncompress finished "$file
     done
     /usr/lib/jdk1.8.0_172/bin/java -Xms1024m -Xmx1024m -cp ./lib/ops-1.1-SNAPSHOT.jar com.killxdcj.aiyawocao.ops.ESReIndexUtils index -e es.host:9620 -p $datedir -i metadata-v2 -t metadata-v2 -b 500
-    echo "reindex finished "$date
-    echo "zip metadata start "$date
+    echo `date`": reindex finished "$date
+    echo `date`": zip metadata start "$date
     rm -rf "metadata."$date"/*"
     mv logs/metadata/* "metadata."$date"/"
     zip -q -r "/data/metadata/archive-new/medata."$date".zip" "metadata."$date
-    echo "zip metadata finished "$date
+    echo `date`": zip metadata finished "$date
     rm -rf $datedir
 done

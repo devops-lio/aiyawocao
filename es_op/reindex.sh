790583cfd88bc7ad4ccb0cb02b819597a3675a0f
caojianhua <caojianhua@bytedance.com>
2018-10-02 16:59:21 +0800

    bug fix
    Change-Id: I02dfca1fea397377c40e64ae6fb4700dd595b86b

--- es_op/reindex.sh
+++ es_op/reindex.sh
@@ -2,6 +2,7 @@
 dates=`ls /data/metadata/archive/human/ | awk -F '.' '{print $2}' | sort | uniq`
 for date in $dates
 do
+    unalias rm
     echo "reindex start "$date
     datedir="metadata."$date
     mkdir -p $datedir
@@ -14,6 +15,8 @@ do
     /usr/lib/jdk1.8.0_172/bin/java -Xms1024m -Xmx1024m -cp ./lib/ops-1.1-SNAPSHOT.jar com.killxdcj.aiyawocao.ops.ESReIndexUtils index -e es.host:9620 -p $datedir -i metadata-v2 -t metadata-v2 -b 500
     echo "reindex finished "$date
     echo "zip metadata start "$date
+    rm -rf "metadata."$date"/*"
+    mv logs/metadata/* "metadata."$date"/"
     zip -q -r "/data/metadata/archive-new/medata."$date".zip" "metadata."$date
     echo "zip metadata finished "$date
     rm -rf $datedir
